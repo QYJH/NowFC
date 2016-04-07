5b9f70f7aff7aa63cb12b4f47562d3d2fa1f061b
YJH <376128077@qq.com>
2016-04-07 15:44:17 +0800

    初始化项目

---
+++ githubText/githubText/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  githubText
+//
+//  Created by YJH on 16/4/7.
+//  Copyright © 2016年 YJH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
